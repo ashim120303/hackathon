5b56d7a061325c481daac65ccc4f00e33f88a18c
ashim120303 <ashim030312@gmail.com>
2023-11-04 02:29:15 +0600

    Login page

---
+++ src/main/java/com/pack/project/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.pack.project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String reg(){
+        return "signup";
+    }
+}
