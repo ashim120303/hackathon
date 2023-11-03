762ae44e9d3240dddad988a1ae6c8714d0542cf3
ashim120303 <ashim030312@gmail.com>
2023-11-03 20:39:22 +0600

    Start

---
+++ src/main/java/com/pack/project/controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.pack.project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
