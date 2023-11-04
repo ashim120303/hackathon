ae82b00fd7d4367cb4a245974bdee0c4634fc96f
ashim120303 <ashim030312@gmail.com>
2023-11-05 00:34:10 +0600

    Some Fixes

--- src/main/java/com/pack/project/services/TenderService.java
+++ src/main/java/com/pack/project/services/TenderService.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class TenderService {
 
     private final TenderRepository tenderRepository;
-    private static final String BASE_URL = "https://zakupki.okmot.kg/popp/view/order/list.xhtml?page=";
+    private static final String BASE_URL = "https://zakupki.okmot.kg/popp/view/order/list.xhtml?page=1";
 
     @Autowired
     public TenderService(TenderRepository tenderRepository) {
@@ -23,48 +23,43 @@ public class TenderService {
     }
 
     public void parseAndSaveTenders() {
-        int pageNumber = 1;
-        while (true) {
-            try {
-                Document doc = Jsoup.connect(BASE_URL + pageNumber).get();
-                Element table = doc.select("table").get(1);
+        try {
+            Document doc = Jsoup.connect(BASE_URL).get();
+            Element table = doc.select("table").get(1);
 
-                if (table == null) {
-                    break; // Выходим из цикла, если таблица не найдена
-                }
-
-                Elements rows = table.select("tr");
-                if (rows.size() <= 1) {
-                    break; // Выходим из цикла, если нет данных для обработки
-                }
-
-                for (int i = 1; i < rows.size(); i++) {
-                    Element row = rows.get(i);
-                    Elements cells = row.select("td");
-
-                    String number = cells.get(0).ownText().trim();
-
-                    // Проверка, существует ли уже запись с таким number
-                    if (!tenderRepository.existsByNumber(number)) {
-                        Tender tender = new Tender();
-                        tender.setNumber(number);
-                        tender.setCompanyName(cells.get(1).ownText().trim());
-                        tender.setPurchaseName(cells.get(3).select("span.nameTender").text().trim());
-                        tender.setPlannedAmount(cells.get(6).ownText().trim());
-                        tender.setDatePublished(cells.get(7).ownText().trim());
-                        tender.setBidsSubmissionDeadline(cells.get(8).ownText().trim());
-
-                        tenderRepository.save(tender);
-                    }
-                    // Если запись с таким number уже существует, она не будет сохранена
-                }
-
-                pageNumber++; // Переход к следующей странице
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                break; // В случае ошибки прерываем цикл
+            if (table == null) {
+                return; // Возвращаемся из метода, если таблица не найдена
             }
+
+            Elements rows = table.select("tr");
+            if (rows.size() <= 1) {
+                return; // Возвращаемся из метода, если нет данных для обработки
+            }
+
+            for (int i = 1; i < rows.size(); i++) {
+                Element row = rows.get(i);
+                Elements cells = row.select("td");
+
+                String number = cells.get(0).ownText().trim();
+
+                // Проверка, существует ли уже запись с таким number
+                if (!tenderRepository.existsByNumber(number)) {
+                    Tender tender = new Tender();
+                    tender.setNumber(number);
+                    tender.setCompanyName(cells.get(1).ownText().trim());
+                    tender.setPurchaseName(cells.get(3).select("span.nameTender").text().trim());
+                    tender.setPlannedAmount(cells.get(6).ownText().trim());
+                    tender.setDatePublished(cells.get(7).ownText().trim());
+                    tender.setBidsSubmissionDeadline(cells.get(8).ownText().trim());
+
+                    tenderRepository.save(tender);
+                }
+                // Если запись с таким number уже существует, она не будет сохранена
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // В случае ошибки выводим stack trace и завершаем метод
         }
     }
 }
