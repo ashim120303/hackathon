398cd225e3e39df17bb0dbb36b31e1e7fbbbbee4
ashim120303 <ashim030312@gmail.com>
2023-11-04 16:05:41 +0600

    Full parse

--- src/main/java/com/pack/project/services/TenderService.java
+++ src/main/java/com/pack/project/services/TenderService.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class TenderService {
 
     private final TenderRepository tenderRepository;
+    private static final String BASE_URL = "https://zakupki.okmot.kg/popp/view/order/list.xhtml?page=";
 
     @Autowired
     public TenderService(TenderRepository tenderRepository) {
@@ -21,32 +22,42 @@ public class TenderService {
     }
 
     public void parseAndSaveTenders() {
-        try {
-            Document doc = Jsoup.connect("https://zakupki.okmot.kg/popp/view/order/list.xhtml").get();
-            Element table = doc.select("table").get(1);
+        int pageNumber = 1;
+        while (true) {
+            try {
+                Document doc = Jsoup.connect(BASE_URL + pageNumber).get();
+                Element table = doc.select("table").get(1);
 
-            if (table != null) {
+                if (table == null) {
+                    break; // Выходим из цикла, если таблица не найдена
+                }
+
                 Elements rows = table.select("tr");
+                if (rows.size() <= 1) {
+                    break; // Выходим из цикла, если нет данных для обработки
+                }
 
-                // Пропустить первую строку с заголовками
                 for (int i = 1; i < rows.size(); i++) {
                     Element row = rows.get(i);
                     Elements cells = row.select("td");
 
                     Tender tender = new Tender();
-                    // Используйте .ownText() вместо .text(), чтобы получить только текст вне тегов <span>
                     tender.setNumber(cells.get(0).ownText());
                     tender.setCompanyName(cells.get(1).ownText());
-                    tender.setPurchaseName(cells.get(3).select("span.nameTender").text()); // Предположим, что название закупки всегда внутри span с классом nameTender
+                    tender.setPurchaseName(cells.get(3).select("span.nameTender").text());
                     tender.setPlannedAmount(cells.get(6).ownText());
                     tender.setDatePublished(cells.get(7).ownText());
                     tender.setBidsSubmissionDeadline(cells.get(8).ownText());
 
                     tenderRepository.save(tender);
                 }
+
+                pageNumber++; // Переход к следующей странице
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                break; // В случае ошибки прерываем цикл
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
