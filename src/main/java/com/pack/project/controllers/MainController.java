dc2486f2c3794f76836e27a3a50bd6253245171c
ashim120303 <ashim030312@gmail.com>
2023-11-04 22:41:09 +0600

    Take data from db.

--- src/main/java/com/pack/project/controllers/MainController.java
+++ src/main/java/com/pack/project/controllers/MainController.java
@@ -1,12 +1,24 @@
 package com.pack.project.controllers;
 
+import com.pack.project.models.Tender;
+import com.pack.project.repos.TenderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private TenderRepository tenderRepository;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Tender> tenders = tenderRepository.findAll();
+        model.addAttribute("tenders", tenders);
         return "index";
     }
 }
